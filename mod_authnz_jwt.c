374bf5cd955399e5937077b8996117e89da271a2
Ilia Choly <ilia.choly@gmail.com>
2015-11-22 19:23:21 -0500

    per directory configuration

--- mod_authnz_jwt.c
+++ mod_authnz_jwt.c
@@ -25,40 +25,75 @@ typedef struct {
     size_t key_length;
 } example_config;
 
-static example_config config;
+const char *example_set_key(cmd_parms *cmd, void *cfg, const char *arg);
+const char *example_set_cookie_name(cmd_parms *cmd, void *cfg, const char *arg);
+const char *example_set_claim_name(cmd_parms *cmd, void *cfg, const char *arg);
+static int example_verify_jwt(const char *jwt, example_config *config);
+static int example_handler(request_rec *r);
+static void register_hooks(apr_pool_t *pool);
+static void *create_dir_conf(apr_pool_t *pool, char *context);
+
+static const command_rec example_directives[] = 
+{
+    AP_INIT_TAKE1("exampleKey", example_set_key, NULL, ACCESS_CONF, "Set the HS256 key"),
+    AP_INIT_TAKE1("exampleCookieName", example_set_cookie_name, NULL, ACCESS_CONF, "Cookie name"),
+    AP_INIT_TAKE1("exampleClaimName", example_set_claim_name, NULL, ACCESS_CONF, "Claim name"),
+    { NULL }
+};
+
+module AP_MODULE_DECLARE_DATA   example_module =
+{
+    STANDARD20_MODULE_STUFF,
+    create_dir_conf,    // Per-directory configuration handler
+    NULL,               // Merge handler for per-directory configurations
+    NULL,               // Per-server configuration handler
+    NULL,               // Merge handler for per-server configurations
+    example_directives, // Any directives we may have for httpd
+    register_hooks      // Our hook registering function
+};
+
+void *create_dir_conf(apr_pool_t *pool, char *context)
+{
+  context = context ? context : "(undefined context)";
+  example_config *config = apr_palloc(pool, sizeof(example_config));
+  if (config) {
+    config->claim_name = "name";
+    config->claim_name_is_set = false;
+    config->cookie_name = "jwt";
+    config->cookie_name_is_set = false;
+    config->key = "";
+    config->key_length = 0;
+    config->key_is_set = false;
+  }
+  return config;
+}
 
 const char *example_set_key(cmd_parms *cmd, void *cfg, const char *arg)
 {
-    config.key = arg;
-    config.key_length = strlen(arg);
-    config.key_is_set = true;
+    example_config *config = (example_config*)cfg;
+    config->key = arg;
+    config->key_length = strlen(arg);
+    config->key_is_set = true;
     return NULL;
 }
 
 const char *example_set_cookie_name(cmd_parms *cmd, void *cfg, const char *arg)
 {
-    config.cookie_name = arg; 
-    config.cookie_name_is_set = true;
+    example_config *config = (example_config*)cfg;
+    config->cookie_name = arg; 
+    config->cookie_name_is_set = true;
     return NULL;
 }
 
 const char *example_set_claim_name(cmd_parms *cmd, void *cfg, const char *arg)
 {
-    config.claim_name = arg;
-    config.claim_name_is_set = true;
+    example_config *config = (example_config*)cfg;
+    config->claim_name = arg;
+    config->claim_name_is_set = true;
     return NULL;
 }
 
-static const command_rec example_directives[] = 
-{
-    AP_INIT_TAKE1("exampleKey", example_set_key, NULL, RSRC_CONF, "Set the HS256 key"),
-    AP_INIT_TAKE1("exampleCookieName", example_set_cookie_name, NULL, RSRC_CONF, "Cookie name"),
-    AP_INIT_TAKE1("exampleClaimName", example_set_claim_name, NULL, RSRC_CONF, "Claim name"),
-    { NULL }
-};
-
-
-static int example_verify_jwt(const char *jwt) 
+static int example_verify_jwt(const char *jwt, example_config *config) 
 {
     int rc = 0;
 
@@ -70,8 +105,8 @@ static int example_verify_jwt(const char *jwt)
 
     jose_key_t key;
     key.alg_type = HS256;
-    key.key = strdup(config.key);
-    key.k_len = config.key_length;
+    key.key = strdup(config->key);
+    key.k_len = config->key_length;
 
     if (jose_add_key(&ctx, key)) {
         rc = 1;
@@ -106,6 +141,11 @@ static int example_handler(request_rec *r)
      */
     if (!r->handler || strcmp(r->handler, "example-handler")) return (DECLINED);
 
+    example_config *config = (example_config*) ap_get_module_config(r->per_dir_config, &example_module);
+    if (!config) {
+      goto OUT;
+    }
+
     // set the content type
     ap_set_content_type(r, "application/json");
 
@@ -116,23 +156,10 @@ static int example_handler(request_rec *r)
         goto OUT;
     }
 
-    ap_rprintf(r, "get headers worked<br />");
-
-    apr_table_t *cookies = apr_table_make(r->pool, 0);
-    if (!cookies) {
-       goto OUT;
+    const char *jwt_text = cookies_lookup(cookies_text, config->cookie_name, r->pool);
+    if (!jwt_text) {
+        goto OUT;
     }
-
-    ap_rprintf(r, "make table worked<br />");
-
-    int res = cookies_load(cookies_text, cookies, r->pool);
-    if (res) {
-       goto OUT;
-    }
-
-    apr_table_do(example_show_cookies, r, cookies, NULL);
-
-    ap_rprintf(r, "load cookies worked<br />");
 
     // Get the JWT from the cookie
     /* const char *jwt = cookies_lookup(cookies, config.cookie_name, r->pool); */
@@ -140,19 +167,14 @@ static int example_handler(request_rec *r)
     /*     goto OUT; */
     /* } */
 
-    const char *jwt = apr_table_get(cookies, "jwt");
-    if (!jwt) {
-        goto OUT;
-    }
-
-    if (example_verify_jwt(jwt)) {
+    if (example_verify_jwt(jwt_text, config)) {
         ap_rprintf(r, "Not Authenticated");
     } else {
         ap_rprintf(r, "Is Authenticated");
     }
 
-    /* json_t *head, *claims, *name; */
-    /*  */
+    json_t *head, *claims, *name;
+
     /* if (jwt_split(jwt, &head, &claims)) { */
     /*     goto OUT; */
     /* } */
@@ -182,22 +204,7 @@ OUT:
 /* register_hooks: Adds a hook to the httpd process */
 static void register_hooks(apr_pool_t *pool) 
 {
-    config.claim_name = "name";
-    config.cookie_name = "jwt";
-    config.key = "";
-    config.key_length = 0;
     /* Hook the request handler */
     ap_hook_handler(example_handler, NULL, NULL, APR_HOOK_LAST);
 }
 
-
-module AP_MODULE_DECLARE_DATA   example_module =
-{
-    STANDARD20_MODULE_STUFF,
-    NULL,               // Per-directory configuration handler
-    NULL,               // Merge handler for per-directory configurations
-    NULL,               // Per-server configuration handler
-    NULL,               // Merge handler for per-server configurations
-    example_directives, // Any directives we may have for httpd
-    register_hooks      // Our hook registering function
-};
